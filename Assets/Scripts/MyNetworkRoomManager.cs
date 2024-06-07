9a9929496b6bec49f0c88e950441b12e27b42621
K-Byeonggon <govleen79@gmail.com>
2024-06-07 10:00:02 +0900

    [feat] Room에 2명 초과로 들어오려고 하면 연결해제 해버리는 기능 추가

--- Assets/Scripts/MyNetworkRoomManager.cs
+++ Assets/Scripts/MyNetworkRoomManager.cs
@@ -5,5 +5,21 @@ using Mirror;
 
 public class MyNetworkRoomManager : NetworkRoomManager
 {
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        maxConnections = 2; // 최대 2명
+    }
 
+    public override void OnRoomServerConnect(NetworkConnectionToClient conn)
+    {
+        // 현재 룸에 연결된 플레이어 수가 최대 인원을 넘는지 확인
+        if (numPlayers >= maxConnections)
+        {
+            conn.Disconnect();
+            return;
+        }
+
+        base.OnRoomServerConnect(conn);
+    }
 }
